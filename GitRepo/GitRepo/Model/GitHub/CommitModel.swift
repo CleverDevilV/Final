f3e3809ce115a0b59d842014e996f6be4f6f5f0e
Viter <ViterDA@icloud.com>
2019-12-08 15:10:48 +0300

    Commit Model and Tests

--- GitRepo/GitRepo/Model/GitHub/CommitModel.swift
+++ GitRepo/GitRepo/Model/GitHub/CommitModel.swift
@@ -8,13 +8,18 @@
 
 import Foundation
 
-// Unit tests ???
+// Unit tests
 
 struct Commits: Codable {
 	let size: Int?
 	let commits: [Commit]?
 }
 
+/**
+Model For [Commit](x-source-tag://Commit).
+Tests - [CommitModelTests](x-source-tag://CommitModelTests).
+*/
+/// - Tag: Commit
 struct Commit: Codable {
 	let sha: String?
 	let message: String?
@@ -22,13 +27,6 @@ struct Commit: Codable {
 	let url: String?
 	var date: String?
 	private var html_url: String?
-	
-	mutating func getUrlOfCommit() -> String? {
-		if self.html_url == nil {
-			self.html_url = self.url?.replacingOccurrences(of: "https://api.", with: "https://").replacingOccurrences(of: "repos/", with: "").replacingOccurrences(of: "commits", with: "commit")
-		}
-		return self.html_url
-	}
 	
 	init(sha: String?, message: String?, author: Author?, url: String?, date: String?) {
 		self.sha = sha
@@ -44,6 +42,18 @@ struct Commit: Codable {
 		let date: String?
 	}
 	
+	/// Function for get url of commit from api for show in WebView
+	mutating func getUrlOfCommit() -> String? {
+		if self.html_url == nil {
+			self.html_url = self.url?.replacingOccurrences(of: "https://api.", with: "https://").replacingOccurrences(of: "repos/", with: "").replacingOccurrences(of: "commits", with: "commit")
+		}
+		return self.html_url
+	}
+	
+	/**
+	Function for get date string
+	- Returns: String with date format "yyyy-MM-dd' 'HH:mm:ss", time zone MSK
+	*/
 	func getCommitDateString() -> String? {
 		let dateFormatter = DateFormatter()
 		dateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ssZ"
